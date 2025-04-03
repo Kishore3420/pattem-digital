4215878ca217a9dfb751de64324b108ca7680dfc
Kishore Kammela <kishorekammela@gmail.com>
2025-04-03 12:32:33 +0530

    feat: add location attributes and office details for multiple branches

---
+++ js/data.js
@@ -0,0 +1,158 @@
+export const locationAttributes = [
+  {
+    country: "India",
+    branch: "Bengaluru",
+    address:
+      "No.293/154/174, 4th Floor, Indiqube Gamma Building, Outer Ring Rd, Opposite to JP Morgan, Bangalore, Karnataka 560103",
+    position: {
+      top: "53.7",
+      left: "70.55"
+    }
+  },
+  {
+    country: "India",
+    branch: "Chennai",
+    address:
+      "No.293/154/172, Outer Ring Road, 4th Floor, Kadubeeansahalli, Chennai, Tamilnadu 560103.",
+    position: {
+      top: "52.75",
+      left: "71.5"
+    }
+  },
+  {
+    country: "USA",
+    branch: "San Jose",
+    address: "240 E Gish Rd, Sandy, CA 95112",
+    position: {
+      top: "41.45",
+      left: "11.75"
+    }
+  },
+  {
+    country: "USA",
+    branch: "Sandy",
+    address: "240 E Gish Rd, San Jose, CA 95112 ",
+    position: {
+      top: "32.9",
+      left: "14.58"
+    }
+  },
+  {
+    country: "Singapore",
+    branch: "Banta Tengeh",
+    address: "No.293/154/172, Outer Ring Road, 4th Floor, Kadubeeansahalli, Banta Tengeh, Singapore 560103.",
+    position: {
+      top: "62.25",
+      left: "77.05"
+    }
+  },
+  {
+    country: "Singapore",
+    branch: "Chia Keng",
+    address: "No.293/154/172, Outer Ring Road, 4th Floor, Kadubeeansahalli, Chia Keng, Singapore 560103.",
+    position: {
+      top: "63.25",
+      left: "78"
+    }
+  }
+]
+
+export const 	cardList = [
+  {
+    title: 'Join our team',
+    contact: 'HR Team | +91-9008862428',
+    email: 'work@pattemdigital.com',
+    iconImg: '../assets/images/profile.svg',
+  },
+  {
+    title: 'Project Enquiry',
+    contact: 'Board-line | +91-9901337558',
+    email: 'business@pattemdigital.com',
+    iconImg: '../assets/images/message.svg',
+  },
+  {
+    title: 'Vendor Enquiry',
+    contact: 'Board-line | +91-9901337558',
+    email: 'marketing@pattemdigital.com',
+    iconImg:'../assets/images/enquiry.svg',
+  },
+  {
+    title: 'Everything else',
+    contact: 'Board-line | +91-9901337558',
+    email: 'hello@pattemdigital.com',
+    iconImg: '../assets/images/thumb.svg',
+  },
+]
+
+export const officeLocations  = [
+  {
+    countryName: 'India',
+    locations: [
+      {
+        name: 'Bangalore',
+        address: 'No.293/154/174, 4th Floor, Indiqube Gamma Building, Outer Ring Rd, Opposite to JP Morgan, Bangalore, Karnataka 560103 ',
+        phoneNumber: '+91-9901337558',
+        googleLocation: '#',
+        gallery: [
+          "../assets/images/gallery1.png",
+          "../assets/images/nodejs.png",
+          "../assets/images/youtube.png",
+          "../assets/images/mern.png",
+        ]
+      },
+      {
+        name: 'Chennai',
+        address: 'No.293/154/172, Outer Ring Road, 4th Floor, Kadubeeansahalli, Chennai, Tamilnadu 560103.',
+        phoneNumber: '+91-9901337558',
+        googleLocation: '#',
+        gallery: [
+          "../assets/images/youtube.png",
+          "../assets/images/mern.png",
+        ]
+      },
+      {
+        name: 'Bangalore',
+        address: 'No.293/154/174, 4th Floor, Indiqube Gamma Building, Outer Ring Rd, Opposite to JP Morgan, Bangalore, Karnataka 560103 ',
+        phoneNumber: '+91-9901337558',
+        googleLocation: '#',
+      },
+      {
+        name: 'Chennai',
+        address: 'No.293/154/172, Outer Ring Road, 4th Floor, Kadubeeansahalli, Chennai, Tamilnadu 560103.',
+        phoneNumber: '+91-9901337558',
+        googleLocation: '#',
+      },
+      {
+        name: 'Bangalore',
+        address: 'No.293/154/174, 4th Floor, Indiqube Gamma Building, Outer Ring Rd, Opposite to JP Morgan, Bangalore, Karnataka 560103 ',
+        phoneNumber: '+91-9901337558',
+        googleLocation: '#',
+      },
+    ],
+    isLoadMore: false
+  },
+  {
+    countryName: 'USA',
+    locations: [
+      {
+        name: 'Sandy',
+        address: '240 E Gish Rd, Sandy, CA 95112 ',
+        phoneNumber: '+91-9901337558',
+        googleLocation: '#',
+      },
+      {
+        name: 'San Jose',
+        address: '240 E Gish Rd, San Jose, CA 95112 ',
+        phoneNumber: '+91-9901337558',
+        googleLocation: '#',
+      },
+      {
+        name: 'San Jose',
+        address: '240 E Gish Rd, San Jose, CA 95112 ',
+        phoneNumber: '+91-9901337558',
+        googleLocation: '#',
+      },
+    ],
+    isLoadMore: false
+  },
+]
